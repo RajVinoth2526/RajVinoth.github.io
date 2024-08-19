21177d52a069996e9bb45ac8b63c434a42c2d24b
RajVinoth2526 <vinothraj2526@gmail.com>
2024-08-19 22:59:54 +0530

    update header component

--- src/app/home/header/header/header.component.ts
+++ src/app/home/header/header/header.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { DataService } from 'src/app/service/dataService/data.service';
+import { getDoc, doc, Firestore, getDocFromCache } from "firebase/firestore";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +14,178 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  signInForm!: FormGroup;
+  signUpForm!: FormGroup;
+  user$!: Observable<any>;
 
-  constructor() { }
+  constructor(private dataService: DataService,
+    private firestore: AngularFirestore,
+    private spinner: NgxSpinnerService,
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+  ) {
+
+  }
 
   ngOnInit(): void {
+    this.fetchData();
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]], // Corrected form control name
+      password: ['', [Validators.required]], // Corrected form control name
+    });
+
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required], // Corrected form control name
+      lastName: ['', Validators.required], // Corrected form control name
+      email: ['', [Validators.required, Validators.email]], // Corrected form control name
+      phone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]], // Corrected form control name
+      country: ['', Validators.required], // Corrected form control name
+      address1: ['', Validators.required], // Corrected form control name
+      address2: [''], // Corrected form control name
+      city: ['', Validators.required], // Corrected form control name
+      state: ['', Validators.required], // Corrected form control name
+      postalCode: ['', Validators.required], // Corrected form control name
+      password: ['', Validators.required], // Corrected form control name
+    });
+
   }
+
+  async fetchData() {
+    try {
+      this.spinner.show();
+      this.dataService.updateLoadingIndicator(true);
+
+      // Check if cached data exists and is still valid
+      let mainShowData = this.dataService.getMainSliderData();
+      let categoriesData = this.dataService.getCategoryData();
+      let Products = this.dataService.getProductsData();
+
+
+
+      const cacheKey = 'cache_timestamp';
+      const cacheTTL = 1000 * 60 * 10; // 1 hour in milliseconds
+
+      const cachedTimestamp = localStorage.getItem(cacheKey);
+      const isCacheValid = cachedTimestamp && (Date.now() - parseInt(cachedTimestamp)) < cacheTTL;
+
+      if (isCacheValid && mainShowData.length && categoriesData.length && Products.length) {
+        // Use the cached data
+        this.dataService.updateMainSliderData(mainShowData);
+        this.dataService.updatecategoryData(categoriesData);
+        this.dataService.updateProductsData(Products);
+        this.spinner.hide();
+        console.log('Using cached data');
+      } else {
+        console.log('Fetching fresh data');
+        // Fetch data from Firestore
+        const sliderShowDataPromise = new Promise<any[]>((resolve, reject) => {
+          this.firestore.collection('products').doc('SliderShow').collection('SliderShow').valueChanges().subscribe({
+            next: (data) => resolve(data as any[]),
+            error: (err) => reject(err)
+          });
+        });
+
+        const categoriesDataPromise = new Promise<any[]>((resolve, reject) => {
+          this.firestore.collection('products').doc('catergories').collection('catergories').valueChanges().subscribe({
+            next: (data) => resolve(data as any[]),
+            error: (err) => reject(err)
+          });
+        });
+
+        const getProductsPromise = new Promise<any[]>((resolve, reject) => {
+          this.firestore.collection('products').doc('product').collection('product').valueChanges().subscribe({
+            next: (data) => resolve(data as any[]),
+            error: (err) => reject(err)
+          });
+        });
+
+
+        // Wait for all promises to resolve
+        mainShowData = await sliderShowDataPromise;
+        categoriesData = await categoriesDataPromise;
+        Products = await getProductsPromise;
+
+        // Cache the data in dataService
+        this.dataService.updateMainSliderData(mainShowData);
+        this.dataService.updatecategoryData(categoriesData);
+        this.dataService.updateProductsData(Products);
+
+        // Update local storage with fresh data and timestamp
+        localStorage.setItem('mainShowData', JSON.stringify(mainShowData));
+        localStorage.setItem('categoriesData', JSON.stringify(categoriesData));
+        localStorage.setItem('Products', JSON.stringify(Products));
+        localStorage.setItem(cacheKey, Date.now().toString());
+        this.spinner.hide();
+      }
+
+      this.dataService.updateLoadingIndicator(false);
+
+    } catch (error) {
+      this.dataService.updateLoadingIndicator(false);
+      console.error('Error fetching data:', error);
+      this.spinner.hide();
+    }
+  }
+
+  async onSubmitSignIn() {
+    if (this.signInForm.invalid) {
+      this.signInForm.markAllAsTouched();
+      return;
+    }
+
+    try {
+      this.spinner.show();
+      const { email, password } = this.signUpForm.value;
+      const result = await this.afAuth.signInWithEmailAndPassword(email, password);
+      if (result) {
+        this.toastr.success('Login Successfully');
+        this.spinner.hide();
+      }
+    } catch (error) {
+      this.spinner.hide();
+      this.toastr.warning('' + error);
+
+    }
+  }
+
+  async onSubmitSignUp() {
+    if (this.signUpForm.invalid) {
+      this.signUpForm.markAllAsTouched();
+      return;
+    }
+
+    const { firstName, lastName, email, phone, country, address1, address2, city, state, postalCode, password } = this.signUpForm.value;
+
+    try {
+      this.spinner.show();
+      // Create user with Firebase Authentication
+      const userCredential = await this.afAuth.createUserWithEmailAndPassword(email, password);
+
+      // Store additional user details in Firestore
+      await this.firestore.collection('users').doc(userCredential.user?.uid).set({
+        firstName,
+        lastName,
+        email,
+        phone,
+        country,
+        address1,
+        address2,
+        city,
+        state,
+        postalCode,
+      });
+      this.spinner.hide();
+      this.toastr.success('SignUp Successfully');
+
+    } catch (error) {
+      this.toastr.warning('SignUp Failed');
+      this.spinner.hide();
+
+    }
+  }
+
+
+
 
 }
